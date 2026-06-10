ec3f71282c0aae7d2f0442f854c41d618c13c7d5
Hamza Hussein <hamzahussain5051@gmail.com>
2026-06-10 08:52:49 +0300

    Create Score of string.py

---
+++ Score of string.py
@@ -0,0 +1,14 @@
+import math
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        summations = 0
+        for i in range(1,len(s)):
+            summations += abs(ord(s[i])-ord(s[i-1]))
+        return summations     
+
+
+        
