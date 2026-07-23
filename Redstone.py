6628aecec731b08a3ad9186acafa23f25dd94567
Hamza Hussein <hamzahussain5051@gmail.com>
2026-07-22 22:50:23 -0700

    Create Redstone.py

---
+++ Redstone.py
@@ -0,0 +1,9 @@
+#flenbar
+t = int(input())
+for i in range(t) :
+    n = int(input())
+    a = list(map(int,input().split()))
+    if len(set(a)) < len(a) :
+        print("YES")
+    else:
+        print("NO")     
